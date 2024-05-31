a550b0a24feebb2d88ca97dbebbeecc072222a53
杰酷 <runjie.xrj@alibaba-inc.com>
2024-05-31 09:59:04 +0800

    修改系统名称

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || '燕喜堂SRM系统'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
